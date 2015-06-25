72c3772527f317fad5c0d7c5ded64fda490b1028
VioletaDimova <vilito_s_usmivka@abv.bg>
2015-06-25 15:59:02 +0300

    Update solution 2-5.php.

--- Homework/2/2-5.php
+++ Homework/2/2-5.php
@@ -1,7 +1,7 @@
 <?php
-$x=1;
+$x=0;
 $y=2;
-if ($x<2 && $x>-2 && $y<2 && $y>-2 or $x==2 or $x==-2 or $y==2 or $y==-2){
+if ($x<=2 && $y<=2 && $x>=-2 && $y>=-2){
 	echo "Point ($x, $y) <br/>";
 	echo "The point is inside K({0,0}, 2).";
 }
