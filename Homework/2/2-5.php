26ff990c8d38ed601cae95076f4c09c7e182696c
VioletaDimova <vilito_s_usmivka@abv.bg>
2015-06-25 14:10:21 +0300

    Update solution 2-5.php.

--- Homework/2/2-5.php
+++ Homework/2/2-5.php
@@ -1,10 +1,12 @@
 <?php
 $x=1;
 $y=2;
-if ($x=2 && $x=-2 && $y=2 && $y=-2 or $x=2 or $x=-2 or $y=2 or $y=-2){
-	echo "The point is inside the circle.";
+if ($x<2 && $x>-2 && $y<2 && $y>-2 or $x==2 or $x==-2 or $y==2 or $y==-2){
+	echo "Point ($x, $y) <br/>";
+	echo "The point is inside K({0,0}, 2).";
 }
 else {
-	echo "The point is outside the circle.";
+	echo "Point ($x, $y) <br/>";
+	echo "The point is outside K({0,0}, 2).";
 }
 ?>
